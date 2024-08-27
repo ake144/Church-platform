1e9647340a1641a21ba2d397fc2519a5141463d3
ake <tamiratake@gmail.com>
2024-08-27 22:13:35 +0300

    news page added

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,13 +1,13 @@
 import Articles from "@/components/articles";
 
-const ArticlesPage = () => {
+const NewsPage = () => {
     return (
             
         <div className=" flex flex-col h-screen items-center justify-center bg-gray-600 ">
-        <h1 className="text-5xl font-serif text-white  my-8">Articles</h1>
+        <h1 className="text-5xl font-serif text-white  my-8">News</h1>
         <Articles />
         </div>
     );
     }
 
-export default ArticlesPage;
+export default NewsPage;
